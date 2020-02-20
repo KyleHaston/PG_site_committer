48813138a04333b77bcef6c35371ba10ba60352e
Kyle Haston <microfarads@gmail.com>
2020-02-19 20:37:49 -0800

    Placeholders

---
+++ main.py
@@ -0,0 +1,21 @@
+# Kyle Haston
+# Feb 2020
+# I want a script to add some information to a file on my server on a daily basis.
+
+from paramiko import *
+import credentials
+
+
+def main():
+    # Open the local file, and read some info.
+    print('asdf')
+
+    # Write it to the other file.
+    print('sadf')
+
+    # Commit the other file.
+    print('qwer')
+
+
+if __name__ == "__main__":
+    main()
