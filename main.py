3fea4e0e02254a7bcac662019fa0bb8eb67cd12e
Kyle Haston <microfarads@gmail.com>
2020-02-20 19:24:16 -0800

    commit for 2020-02-20 test

--- main.py
+++ main.py
@@ -3,32 +3,35 @@
 # I want a script to add some information to a file on my server on a daily basis.
 
 import datetime
-from paramiko import *
+import os
+import sys
 import credentials
 
 
 def main():
-    # Open the local file, and read some info.
+    # Open the input file, and read some info.
     print('asdf')
-    with open('some_data.txt','r') as in_file:
-        # with open('../../PG-Site-Forecast-Buddy/PG-Site-Forecast-Buddy/site_data.py','w') as out_file:
-        with open('site_data.py','w') as out_file:
+    with open('some_data.txt', 'r') as in_file:
+        with open('output.txt', 'w') as out_file:
             data = in_file.readlines()
 
             # Get the date.
             today_str = str(datetime.datetime.today()).split()[0]
             print('    today: ' + today_str)
 
-
-            # Write it to the other file.
+            # If there is data to be written today, write it to the other file.
             for line in data:
                 if today_str in line:
                     print('    writing this to the file: ' + line)
+                    out_file.writelines(data)
 
-            print('    done writing!')
+                    print('    commit the other file: ')  # Commit the other file.
+                    os.system('git commit -am "commit for ' + line + '"')
+                    os.system('git push')
+                    sys.exit(0)
 
-            # Commit the other file.
-            print('    commit the other file: ')
+                else:
+                    print('    not writing anything today!')
 
 
 if __name__ == "__main__":
